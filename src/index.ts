a453080a43bfbbb7be4173442c004c9e48eacf55
Luis Angelo Belmonte <lafbelmonte@gmail.com>
2021-01-05 08:18:59 +0800

    -added linting

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,64 @@
-import R from 'ramda'
+import R from 'ramda';
 
 type Car = {
-    name: string;
-    horsepower: number;
-    dollarValue: number;
-    inStock: boolean;
-}
+  name: string;
+  horsepower: number;
+  dollarValue: number;
+  inStock: boolean;
+};
 
-const cars: Car[] = [
-    {
-        name: 'Aston Martin One-77',
-        horsepower: 750,
-        dollarValue: 1850000,
-        inStock: true,
-    },
-    {
-        name: 'Nissan Skyline GTR',
-        horsepower: 850,
-        dollarValue: 1950000,
-        inStock: true,
-    }
-]
+// const cars: Car[] = [
+//   {
+//     name: 'Aston Martin One-77',
+//     horsepower: 750,
+//     dollarValue: 1850000,
+//     inStock: true,
+//   },
+//   {
+//     name: 'Nissan Skyline GTR',
+//     horsepower: 850,
+//     dollarValue: 1950000,
+//     inStock: true,
+//   },
+// ];
 
-const IsLastInStock = R.compose<Car[], Car, boolean>(R.prop('inStock'), R.last)
+const IsLastInStock = R.compose<Car[], Car, boolean>(R.prop('inStock'), R.last);
 
-console.log(IsLastInStock(cars))
+const averageDollarValue = R.compose<Car[], number[], number>(
+  R.converge(R.divide, [R.sum, R.length]),
+  R.map(R.prop('dollarValue')),
+);
 
-const averageDollarValue = R.compose<Car[], Number[], Number>(R.converge(R.divide, [R.sum, R.length]), R.map(R.prop('dollarValue')))
+const fastestCar = R.compose<Car[], Car[], Car, string, string>(
+  R.flip(R.concat)(' is the fastest'),
+  R.prop('name'),
+  R.last,
+  R.sortBy(R.prop('horsepower')),
+);
 
-console.log(averageDollarValue(cars))
+// const data = [{ acc_1: 1 }, { acc_1: 2 }, { acc_2: 3 }];
 
-const fastestCar = R.compose<Car[], Car[], Car, String, String>(R.flip(R.concat)(' is the fastest'), R.prop('name'), R.last, R.sortBy(R.prop('horsepower')))
+const getEachAccountsTotalPayout = R.compose<any[], any, any, any, any>(
+  R.map(R.sum),
+  R.map(R.pluck(1)),
+  R.groupBy(R.head),
+  R.chain(R.toPairs),
+);
 
-console.log(fastestCar(cars))
+// [ [ 'acc_1', 1 ], [ 'acc_1', 2 ], [ 'acc_2', 3 ] ]
 
-const data = [
-    { acc_1: 1 },
-    { acc_1: 2 },
-    { acc_2: 3 },
-];
+// {
+//     acc_1: [ [ 'acc_1', 1 ], [ 'acc_1', 2 ] ],
+//     acc_2: [ [ 'acc_2', 3 ] ]
+// }
 
-const getEachAccountsTotalPayout = R.compose<any, {}, {}, {}, {}>(R.map(R.sum), R.map(R.pluck(1)), R.groupBy(R.head), R.chain(R.toPairs))
+// { acc_1: [ 1, 2 ], acc_2: [ 3 ] }
 
-console.log(getEachAccountsTotalPayout(data))
+// { acc_1: 3, acc_2: 3 }
 
 export {
-    IsLastInStock,
-    averageDollarValue,
-    fastestCar,
-    getEachAccountsTotalPayout
-}
-
-
-
-
-
-
-
-
-
-
+  IsLastInStock,
+  averageDollarValue,
+  fastestCar,
+  getEachAccountsTotalPayout,
+};
