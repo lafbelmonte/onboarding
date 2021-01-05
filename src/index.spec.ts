0fc14b4fa1430162c3af7b45e0ebcf7e78b278bd
Luis Angelo Belmonte <lafbelmonte@gmail.com>
2021-01-05 10:49:01 +0800

    test(library): updated all tests
    finished tests for 1,2,3,4

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,51 +1,99 @@
-
-
+import { expect } from 'chai';
 import {
-    IsLastInStock,
-    averageDollarValue,
-    fastestCar,
-    getEachAccountsTotalPayout,
-} from './index'
-
-import chai from 'chai'
-const expect = chai.expect
-
-const cars = [
-    {
-        name: 'Aston Martin One-77',
-        horsepower: 750,
-        dollarValue: 1850000,
-        inStock: true,
-    },
-    {
-        name: 'Nissan Skyline GTR',
-        horsepower: 850,
-        dollarValue: 1950000,
-        inStock: true,
-    },
-];
+  IsLastInStock,
+  averageDollarValue,
+  fastestCar,
+  getEachAccountsTotalPayout,
+} from './index';
 
 describe('is last in stock', () => {
-    it("should run the test", () => {
-        expect(IsLastInStock(cars)).to.be.true
-    })
-})
+  const cars = [
+    {
+      name: 'Aston Martin One-77',
+      horsepower: 750,
+      dollarValue: 1850000,
+      inStock: true,
+    },
+    {
+      name: 'Nissan Skyline GTR',
+      horsepower: 850,
+      dollarValue: 1950000,
+      inStock: true,
+    },
+  ];
+  const cars2 = [
+    {
+      name: 'Aston Martin One-77',
+      horsepower: 750,
+      dollarValue: 1850000,
+      inStock: true,
+    },
+    {
+      name: 'Nissan Skyline GTR',
+      horsepower: 850,
+      dollarValue: 1950000,
+      inStock: false,
+    },
+  ];
+
+  it('should return true', () => {
+    expect(IsLastInStock(cars)).to.equal(true);
+  });
+
+  it('should return false', () => {
+    expect(IsLastInStock(cars2)).to.equal(false);
+  });
+});
 
 describe('average dollar value', () => {
-    it("should run the test", () => {
-        expect(averageDollarValue(cars)).to.be.true
-    })
-})
+  const cars = [
+    {
+      name: 'Aston Martin One-77',
+      horsepower: 750,
+      dollarValue: 1850000,
+      inStock: true,
+    },
+    {
+      name: 'Nissan Skyline GTR',
+      horsepower: 850,
+      dollarValue: 1950000,
+      inStock: true,
+    },
+  ];
+
+  it('should return the average', () => {
+    expect(averageDollarValue(cars)).to.equal(1900000);
+  });
+});
 
 describe('fastest car', () => {
-    it("should run the test", () => {
-        expect(fastestCar(cars)).to.be.true
-    })
-})
+  const cars = [
+    {
+      name: 'Aston Martin One-77',
+      horsepower: 750,
+      dollarValue: 1850000,
+      inStock: true,
+    },
+    {
+      name: 'Nissan Skyline GTR',
+      horsepower: 850,
+      dollarValue: 1950000,
+      inStock: true,
+    },
+  ];
+
+  it('should return the faster car', () => {
+    expect(fastestCar(cars)).to.equal('Nissan Skyline GTR is the fastest');
+  });
+});
 
 describe('fastest car', () => {
-    it("should run the test", () => {
-        expect(getEachAccountsTotalPayout(cars)).to.be.true
-    })
-})
+  const data = [{ acc_1: 1 }, { acc_1: 2 }, { acc_2: 3 }];
 
+  it('should run the test', () => {
+    expect(getEachAccountsTotalPayout(data)).to.deep.equal({
+      acc_1: 3,
+      acc_2: 3,
+    });
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -7,21 +7,6 @@ type Car = {
   inStock: boolean;
 };
 
-// const cars: Car[] = [
-//   {
-//     name: 'Aston Martin One-77',
-//     horsepower: 750,
-//     dollarValue: 1850000,
-//     inStock: true,
-//   },
-//   {
-//     name: 'Nissan Skyline GTR',
-//     horsepower: 850,
-//     dollarValue: 1950000,
-//     inStock: true,
-//   },
-// ];
-
 const IsLastInStock = R.compose<Car[], Car, boolean>(R.prop('inStock'), R.last);
 
 const averageDollarValue = R.compose<Car[], number[], number>(
@@ -36,25 +21,12 @@ const fastestCar = R.compose<Car[], Car[], Car, string, string>(
   R.sortBy(R.prop('horsepower')),
 );
 
-// const data = [{ acc_1: 1 }, { acc_1: 2 }, { acc_2: 3 }];
-
 const getEachAccountsTotalPayout = R.compose<any[], any, any, any, any>(
   R.map(R.sum),
   R.map(R.pluck(1)),
   R.groupBy(R.head),
   R.chain(R.toPairs),
 );
-
-// [ [ 'acc_1', 1 ], [ 'acc_1', 2 ], [ 'acc_2', 3 ] ]
-
-// {
-//     acc_1: [ [ 'acc_1', 1 ], [ 'acc_1', 2 ] ],
-//     acc_2: [ [ 'acc_2', 3 ] ]
-// }
-
-// { acc_1: [ 1, 2 ], acc_2: [ 3 ] }
-
-// { acc_1: 3, acc_2: 3 }
 
 export {
   IsLastInStock,
