96252865e1492483bae2dd27115e67500acc02d2
Luis Angelo Belmonte <lafbelmonte@gmail.com>
2021-01-21 08:36:19 +0800

    refactor: test ci

--- koa-crud/src/index.ts
+++ koa-crud/src/index.ts
@@ -3,6 +3,8 @@ import App from './lib/koa/server';
 import vendors from './routes/vendors';
 import auth from './routes/auth';
 
+console.log('test');
+
 const app = new App(5000, [vendors, auth]);
 
 app.start();
