d6a025cec8f2be77527ad39caddffa4da69fed23
Luis Angelo Belmonte <lafbelmonte@gmail.com>
2021-01-21 11:27:32 +0800

    test

--- koa-crud/src/index.ts
+++ koa-crud/src/index.ts
@@ -3,8 +3,6 @@ import App from './lib/koa/server';
 import vendors from './routes/vendors';
 import auth from './routes/auth';
 
-console.log('test');
-
 const app = new App(5000, [vendors, auth]);
 
 app.start();
